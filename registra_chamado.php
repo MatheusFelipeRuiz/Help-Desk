a54d6e4731c66f1a441431e64761bf4c3bfb30c3
MatheusFelipeRuiz <matheus57.ruiz100@gmail.com>
2023-12-28 22:14:29 -0300

    Adicionando o cadastro de chamados no formato JSON para arquivos de texto

---
+++ registra_chamado.php
@@ -0,0 +1,37 @@
+<pre>
+    <?php
+        $titulo = $_POST['titulo'];
+        $categoria = $_POST['categoria'];
+        $descricao = $_POST['descricao'];
+
+        $diretorio = 'chamados';
+        $conteudoArquivo = 'chamado.txt';
+        $caminhoArquivoLeitura  = $diretorio . '/' . $conteudoArquivo;
+
+
+        fclose($arquivo);
+        if(!file_exists($diretorio)){
+            mkdir($diretorio);
+            $caminhoAbsoluto = $diretorio . '/' . $conteudoArquivo;
+            $arquivo = fopen($caminhoAbsoluto, 'a');
+            
+            $objeto = array('titulo' => "$titulo", 'categoria' => "$categoria", 'descricao' => "$descricao");
+            $JSON = json_encode($objeto) . PHP_EOL;
+            
+            fwrite($arquivo,$JSON);
+            fclose($arquivo);    
+        }else {
+            $caminhoAbsoluto = $diretorio . '/' . $conteudoArquivo;
+            $arquivo = fopen($caminhoAbsoluto, 'a');
+            
+            $objeto = array('titulo' => "$titulo", 'categoria' => "$categoria", 'descricao' => "$descricao");
+            $JSON = json_encode($objeto) . PHP_EOL;
+
+            fwrite($arquivo,$JSON);
+            fclose($arquivo);
+        }
+
+        header('Location: consultar_chamado.php');
+        
+    ?>
+</pre>
